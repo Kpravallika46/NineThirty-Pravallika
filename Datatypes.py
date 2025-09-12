0a72827ebc740e929ad9add7cbdda189dc4e1905
K. Baby Pravallika <kpravallikababy@gmail.com>
2025-09-12 15:53:37 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,36 @@
+#integer 
+a=30
+b=50
+c=a+b
+print(c)
+print(type(c))
+
+#boolean
+a="its is very beauty"
+b="its is very handsome"
+y=True
+k=False
+if(len(a)==len(b)):
+      print(a)
+else:
+    print(b)
+
+#float
+a="its is very beauty"
+b="its is very handsome"
+y=True
+k=False
+if(len(a)==len(b)):
+      print(a)
+else:
+    print(b)
+
+#string
+a='ammu'
+b='adhi'
+c=a+b
+print(c)
+print(type(a))
+
+
+
