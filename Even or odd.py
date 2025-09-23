03b75a884b5c9b1ae672403a0546b3d85ff6efa2
K. Baby Pravallika <kpravallikababy@gmail.com>
2025-09-23 11:41:04 +0530

    Add files via upload

---
+++ Even or odd.py
@@ -0,0 +1,9 @@
+#even or odd
+def even_odd(number):
+    if number%2==0:
+        print (number,"even numbers")
+    else:
+        print (number,"odd numbers")
+even_odd(5)        
+              
+            
