500d5a07c6b9c28a9bea0b708d3842e8096bce82
K. Baby Pravallika <kpravallikababy@gmail.com>
2025-09-23 11:14:20 +0530

    Add files via upload

--- Numsprime.py
+++ Numsprime.py
@@ -1,12 +1,16 @@
+ #numsprime.py
+ num = int(input("Enter a number: "))
 
-num = int(input("Enter a number: "))
-
-if num > 1:   # prime numbers are greater than 1
-    for i in range(2, num):
-        if num % i == 0:
-            print(num, "is not a prime number")
-            break
-    else:
-        print(num, "is a prime number")
+if num <= 1:
+    print(f"{num} is not a prime number")
 else:
-    print(num, "is not a prime number")
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
